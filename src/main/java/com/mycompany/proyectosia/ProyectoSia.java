24653555970c1c641c1342d544977b037badac8f
Flavio Luna <flunavio648@gmail.com>
2025-08-26 22:11:17 -0400

    Creacion de clase pasajero con sus setters y getters

---
+++ src/main/java/com/mycompany/proyectosia/ProyectoSia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectosia;
+
+/**
+ *
+ * @author gueon
+ */
+public class ProyectoSia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
